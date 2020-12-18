3bf544599ae513a7d453e9fe92a1d4b6e0d6a5c9
HidekiShiba <hshiba033@gmail.com>
2020-12-18 20:47:10 +0900

    [Add]　フォロー/フォロワー画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,13 @@ class UsersController < ApplicationController
     @user = current_user
     @book = Book.new
   end
-  
+
   def show
     @user = User.find(params[:id])
     @book = Book.new
     @books = @user.books.order(created_at: :desc)
   end
-  
+
   def edit
     @user = User.find(params[:id])
     if @user == current_user
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user)
     end
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -29,7 +29,15 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-  
+
+  def following
+    @user = User.find(params[:id])
+  end
+
+  def followers
+    @user = User.find(params[:id])
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,15 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   devise_for :users, controllers: { registrations: 'users/registrations',
                                     sessions: 'users/sessions' }
-  resources :users, only: [:show, :index, :edit, :update]
+  resources :users, only: [:show, :index, :edit, :update] do
+    member do
+      get :following, :followers
+    end
+  end
+
   post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
   post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
