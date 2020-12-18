4739e7eff1e2ae1b61715848b1338c6f40d16da9
HidekiShiba <hshiba033@gmail.com>
2020-12-18 17:35:00 +0900

    [Add] フォロー機能/index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations',
                                     sessions: 'users/sessions' }
   resources :users, only: [:show, :index, :edit, :update]
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
